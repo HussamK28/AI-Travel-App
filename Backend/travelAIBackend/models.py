0b3f71f9c7f716f01b4dd49bb69f06b5bbbafb5b
Hussam Khan <HussamK2812@gmail.com>
2025-02-12 18:20:47 +0000

    Added a database model for users database

--- Backend/travelAIBackend/models.py
+++ Backend/travelAIBackend/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 
 class users(models.Model):
-    firstName = models.CharField(max_length=200)
+    firstName = models.CharField(max_length=200),
+    surname = models.CharField(max_length=200),
+    email = models.CharField(max_length=200),
+    password = models.CharField(max_length=200)
