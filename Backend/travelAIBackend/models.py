be37497b47b3c71404edaba2f8e7e8102b157e3e
Hussam Khan <HussamK2812@gmail.com>
2025-02-14 12:18:51 +0000

    Added defaults for database and applied migrations

--- Backend/travelAIBackend/models.py
+++ Backend/travelAIBackend/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Below creates a database model for my users tables to store login and registration details for my users.
 
 class users(models.Model):
-    firstName = models.CharField(max_length=200)
-    surname = models.CharField(max_length=200)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=200)
+    firstName = models.CharField(max_length=200, default="John")
+    surname = models.CharField(max_length=200, default="Doe")
+    email = models.CharField(max_length=200, default="JohnDoe123@gmail.com")
+    password = models.CharField(max_length=200, default="Calculator123!")
