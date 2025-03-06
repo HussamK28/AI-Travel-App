5a929719ad32555b0a40badb199f04deb1f36b69
Hussam Khan <HussamK2812@gmail.com>
2025-03-06 12:20:34 +0000

    Set up basic search flight page

--- Frontend/src/Flights.js
+++ Frontend/src/Flights.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 const Flights = () => {
+    const [depAirport, setDepAirport] = useState("")
+    const [arrAirport, setArrAirport] = useState("")
+    const [depDate, setDepDate] = useState("")
+    const [arrDate, setArrDate] = useState("")
+
+    const newDepAirport = (e) => {
+        setDepAirport(e.target.value)
+    }
+
+    const newArrAirport = (e) => {
+        setArrAirport(e.target.value)
+    }
+
+    const newDepDate = (e) => {
+        setDepDate(e.target.value)
+    }
+
+    const newArrDate = (e) => {
+        setArrDate(e.target.value)
+    }
     return (
-        <h1>Hussam Khan</h1>
+        <div className="container">
+            <h1>Search for Flights</h1>
+            <form>
+                <div className="departure-airport">
+                    <label>From:</label>
+                    <input type="text" 
+                    placeholder="Enter 3 Letters" 
+                    value={depAirport}
+                    onChange={newDepAirport} 
+                    required></input>
+
+                </div>
+                <div className="arrival-airport">
+                    <label>To:</label>
+                    <input type="text"
+                    placeholder="Enter 3 Letters"
+                    value={arrAirport}
+                    onChange={newArrAirport} 
+                    required></input>
+                </div>
+
+                <div className="departure-date">
+                    <label>Departure Date:</label>
+                    <input type="date"
+                    value={depDate}
+                    onChange={newDepDate}
+                    required></input>
+                </div>
+
+                <div className="arrival-date">
+                    <label>Arrival Date:</label>
+                    <input type="date"
+                    value={arrDate}
+                    onChange={newArrDate}
+                    required></input>
+
+                </div>
+
+                <div className="button">
+                <button className="submit-button" 
+                type="submit">Find Flights
+                </button>
+            </div>
+            </form>
+
+        </div>
+
     )
 
 }
