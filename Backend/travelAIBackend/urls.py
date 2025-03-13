c85643543abaed0f4a7cd1e01bba04b128c42937
Hussam Khan <HussamK2812@gmail.com>
2025-03-13 15:40:38 +0000

    Automatically creates a new API token

--- Backend/travelAIBackend/urls.py
+++ Backend/travelAIBackend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import addUserToDatabase, accountExists
+from .views import addUserToDatabase, accountExists, viewFlightsAPIToken
 
 urlpatterns = [
     path("addUserToDatabase/", addUserToDatabase, name="addUserToDatabase"),
-    path("accountExists/", accountExists, name="accountExists") 
+    path("accountExists/", accountExists, name="accountExists"),
+    path("viewFlightsAPIToken/", viewFlightsAPIToken, name="viewFlightsAPIToken"),
 ]
--- Backend/travelAIBackend/views.py
+++ Backend/travelAIBackend/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import users
 from .serializers import userConverter
+import requests
+import time
+from django.http import JsonResponse
+from django.conf import settings
 
 @api_view(['POST'])
 def addUserToDatabase(request):
@@ -23,3 +27,34 @@ def accountExists(request):
         return Response({"message": "Login Successful!"}, status=status.HTTP_202_ACCEPTED)
     else:
         return Response({"message": "Email or password incorrect, please try again!"}, status=status.HTTP_400_BAD_REQUEST)
+
+apiAccessToken = None
+apiExpiry = 0
+
+
+def getFlightsAPIToken():
+    if apiAccessToken and apiExpiry > time.time(): # Checks if there is still time remaining on token
+        return apiAccessToken
+    else:
+        url = "https://test.api.amadeus.com/v1/security/oauth2/token" # URL to receive token
+        APIdata = { # API Key and secret
+            "grant_type":"client_credentials",
+            "client_id": settings.flightsAPIKey,
+            "client_secret": settings.flightsAPISecret
+        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        response = requests.post(url, data=APIdata, headers=headers) # sends the above information to the server to see if we can receive it.
+        if response.status_code == 200: # This means response is ok and valid
+            data = response.json() # Turns server response into a JSON file
+            apiAccessToken = data["access_token"] # Updates new access token
+            apiExpiry = time.time + data["expires_in"] # Updates expiry time
+            return data["access_token"]
+        else:
+            return None
+
+def viewFlightsAPIToken():
+    token = getFlightsAPIToken()
+    if token:
+        return JsonResponse({"access_token": token}, status=200)
+    else:
+        return JsonResponse({"error": "Failed to get token"}, status=500)
