e8c8851c776f4d104d47b3e9a7524367bcd96062
Hussam Khan <HussamK2812@gmail.com>
2025-04-19 15:30:48 +0400

    Create Map.js

---
+++ Frontend/src/Map.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+// imports modules from Google Map's API that we need such as the map, its markers and autocomplete any location searches
+import { GoogleMap, LoadScript, Marker, Autocomplete } from '@react-google-maps/api';
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+
+
+function Map() {
+    // This function sets the size of the map
+const containerStyle = {
+    width: '100%',
+    height: '800px'
+  };
+  
+  // The default centre of the map is London 
+  const defaultCentre = {
+    lat: 51.5074456,
+    lng: -0.1277653
+  };
+
+  const [currentCentre, setCurrentCentre] = useState(defaultCentre)
+  const [locations, setLocations] = useState([])
+  const [searchResults, setSearchResult] = useState()
+
+
+
+
+  return (
+    <LoadScript googleMapsApiKey={apiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={defaultCentre}
+        zoom={12}
+      >
+        <Marker position={defaultCentre} />
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default Map;
