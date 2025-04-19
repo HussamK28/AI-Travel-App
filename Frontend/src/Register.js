af366c306b9eabb7baf0f2ccf884c187f55f3070
Hussam Khan <HussamK2812@gmail.com>
2025-04-19 11:20:15 +0400

    Moved Register.css into new Styles folder

--- Frontend/src/Register.js
+++ Frontend/src/Register.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import './Register.css'
+import './Styles/Register.css'
 import axios from "axios";
 const Register = () => {
     const [firstName, setFirstName] = useState("");
