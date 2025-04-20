840bd474d6fd1a7b0c498918537dfd8029656e01
Hussam Khan <HussamK2812@gmail.com>
2025-04-20 15:12:54 +0400

    Update flightsAPI.js

--- Frontend/src/APIs/flightsAPI.js
+++ Frontend/src/APIs/flightsAPI.js
@@ -1,22 +1,15 @@
-import { useEffect, useState } from "react";
-export function GenerateNewToken() {
-    const [token, setToken] = useState(null);
-
-    useEffect(() => {
-        const fetchToken = async () => {
-            try {
-                const response = await fetch("http://127.0.0.1:8000/viewFlightsAPIToken/");
-                const data = await response.json();
-                if (data.access_token) {
-                    setToken(data.access_token);
-                }
-            } catch (error) {
-                console.error("Error fetching token:", error);
-            }
-        };
-
-        fetchToken();
-    }, []);
-
-    return token;
-}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+export const GenerateNewToken = async () => {
+    const response = await axios.post("https://test.api.amadeus.com/v1/security/oauth2/token", {
+      grant_type: "client_credentials",
+      client_id: "JiRVF50khDdJNdeZG7Pch75SG70PmGFz",
+      client_secret: "H3k65vKUAXZbY2rA",
+    }, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
+  
+    return response.data.access_token;
+  };
+  
+  export default GenerateNewToken
